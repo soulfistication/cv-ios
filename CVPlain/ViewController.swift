bef376610f38dd806c61a9724c9d21ba7e07ab53
Ivan Almada <ivan.almada@green-light.com>
2018-05-07 20:34:59 +0200

    Initial commit.

---
+++ CVPlain/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CVPlain
+//
+//  Created by MacBook Pro on 5/7/18.
+//  Copyright © 2018 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: UIViewController
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
